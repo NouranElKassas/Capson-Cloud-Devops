7157879766eb65c3232d78ea822a0e0bab4c588d
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-06-15 14:44:19 +0200

    Create run_kubernetes.sh

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+capstone
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=capstone
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run $dockerpath --image=nouranelkassas/capstone --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pod
+
+# Step 4:
+# Forward the container port to a host
+kubectl expose deployment capstone --type=LoadBalancer --port=80
+minikube service capstone
