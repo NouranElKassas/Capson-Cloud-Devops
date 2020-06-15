a1938cf5108ea938d020647e6a93db9f634db025
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-06-15 14:40:06 +0200

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=capstone .
+
+# Step 2: 
+# List docker images
+docker images ls
+# Step 3: 
+# Run flask app
+docker run -d -p 80:80 capstone
