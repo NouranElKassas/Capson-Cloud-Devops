b499ec69ca7b8bcb2080de1c2897c2633e49c626
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-06-18 22:10:17 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -6,16 +6,21 @@
 # This is your Docker ID/path
 # dockerpath=<>
 dockerpath=/usr/bin/docker
+docker run -d --restart=always -e DOMAIN=cluster --name capstone -p 80:80 capstone
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run $dockerpath --image=nouranelkassas/capstone --port=80
+kubectl run deployment --image=capstone --port=80
 
 # Step 3:
 # List kubernetes pods
 kubectl get pod
 
+#add domains to env
+# add env to capstone
+kubectl set env deployment/capstone DOMAIN=cluster
+
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment capstone --type=LoadBalancer --port=80
+kubectl expose deployment capstone --type=LoadBalancer --port=80 --name=capstone-http
 minikube service capstone
